c9f4c554542fdd048287b6cefa96534f00aa3309
John Cadena <cadena.john.85@gmail.com>
2019-08-12 16:56:47 -0500

    remove extraneous comments from for_loops.js

--- js/for_loops.js
+++ js/for_loops.js
@@ -9,29 +9,27 @@
         }
     }
 
+    showMultiplicationTable(7);
+
     //Problem #3
 
-    //for loop runs ten times
-    //each time, a new random # 20 - 200 is generated
-    //each time, determine if random is even or odd
-
-    // for (var j = 0; j <10; j++) {
-    //     //Generating random numbers where minimum is not 1:
-    //     // https://stackoverflow.com/questions/4959975/generate-random-number-between-two-numbers-in-javascript
-    //     var randomNumber = Math.floor(Math.random() * (200 - 20 + 1)) + 20;
-    //     if (randomNumber % 2 === 0) {
-    //         console.log(randomNumber + " is even.");
-    //     } else {
-    //         console.log(randomNumber + " is odd.");
-    //     }
-    // }
+    for (var j = 0; j <10; j++) {
+        //Generating random numbers where minimum is not 1:
+        // https://stackoverflow.com/questions/4959975/generate-random-number-between-two-numbers-in-javascript
+        var randomNumber = Math.floor(Math.random() * (200 - 20 + 1)) + 20;
+        if (randomNumber % 2 === 0) {
+            console.log(randomNumber + " is even.");
+        } else {
+            console.log(randomNumber + " is odd.");
+        }
+    }
 
 
     //Problem #4
 
-    // for (var k = 1; k < 10; k++) {
-    //     console.log(k.toString().repeat(k));
-    // }
+    for (var k = 1; k < 10; k++) {
+        console.log(k.toString().repeat(k));
+    }
 
 
             // for (var j = 1; j <10; j++) {
@@ -64,9 +62,9 @@
 
     //Problem #5
 
-    // for (var m = 100; m > 0; m -= 5 ) {
-    //     console.log(m);
-    // }
+    for (var m = 100; m > 0; m -= 5 ) {
+        console.log(m);
+    }
 
 
 
