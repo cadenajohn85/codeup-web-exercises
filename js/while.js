44dc7208d84005a0022cb305ee7d522342dfef09
John Cadena <cadena.john.85@gmail.com>
2019-08-12 12:32:34 -0500

    save progress on while.js

--- js/while.js
+++ js/while.js
@@ -2,6 +2,28 @@
 
     "use strict";
 
+    // var x = 2;
+    // while (x < 70000) {
+    //     console.log(x);
+    //     x *= 2;
+    // }
 
+        // Generate total # of cones (50 - 100)
+    // Generate # of cones sold this round (2 - 6)
+    // Decide if there are enough cones to sell
+    // Subtract sold from total cones
+    // Decide if there are still cones available
 
+    var allCones = Math.floor(Math.random() * 50) + 50;
+    console.log("You need to sell " + allCones + " cones today.");
+    while (allCones > 0) {
+        var conesThisRound = Math.floor(Math.random() * 5) + 1;
+        console.log("May I please have " + conesThisRound + " cones?");
+        if (conesThisRound > allCones) {
+            continue;
+        }
+        allCones = allCones - conesThisRound;
+        console.log("You have " + allCones + " cones remaining.");
+    }
+    console.log("You've sold all your cones! Time to go home.");
 })();
