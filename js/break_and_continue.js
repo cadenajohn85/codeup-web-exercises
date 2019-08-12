003f3845dd4190897535a3491aa0f2a59942c306
John Cadena <cadena.john.85@gmail.com>
2019-08-12 16:45:42 -0500

    remove extraneous comments from break_and_continue.js

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -3,21 +3,6 @@
 
     //Problem #2
 
-    //prompt use for number
-    //check if valid range
-    //check if odd
-
-       // var userNumber = prompt("Please enter an odd number between 1 and 50.");
-       //  console.log(userNumber);
-       //  while (userNumber < 1 || userNumber > 50) {
-       //      //  || userNumber % 2 !== -1
-       //      if (parseFloat(userNumber) % 2 === -1 ) {
-       //          break;
-       //      }
-       //      userNumber = prompt("I'm sorry, that was not a valid input.\n" +
-       //          "Please enter an odd number between 1 and 50.");
-       //  }
-
     var userNumber = -1;
 
     while (userNumber !== 0) {
@@ -27,16 +12,14 @@
         }
     }
 
+    //Problem #3
+
     for (var i = 1; i < 50; i += 2) {
-        //console.log("i = " + i + ", userNumber = " + userNumber);
         if (i === userNumber) {
             console.log ("Yikes! Skipping number: " + userNumber);
             continue;
         }
         console.log("Here is an odd number: " + i);
     }
-
-
-
 
 })();
