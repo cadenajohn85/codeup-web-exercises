d092a0f4005057ffa8a1281ab5d9d5583a5126d5
John Cadena <cadena.john.85@gmail.com>
2019-08-12 14:39:55 -0500

    refactor ice cream problem to use necessary do-while loop instead of while loop

--- js/for_loops.js
+++ js/for_loops.js
@@ -11,16 +11,16 @@
     //each time, a new random # 20 - 200 is generated
     //each time, determine if random is even or odd
 
-    for (var i = 0; i <10; i++) {
-        //Generating random numbers where minimum is not 1:
-        // https://stackoverflow.com/questions/4959975/generate-random-number-between-two-numbers-in-javascript
-        var randomNumber = Math.floor(Math.random() * (200 - 20 + 1)) + 20;
-        //console.log(i + " || " + randomNumber);
-        if (randomNumber % 2 === 0) {
-            console.log(randomNumber + " is even.");
-        } else {
-            console.log(randomNumber + " is odd.");
-        }
-    }
+    // for (var i = 0; i <10; i++) {
+    //     //Generating random numbers where minimum is not 1:
+    //     // https://stackoverflow.com/questions/4959975/generate-random-number-between-two-numbers-in-javascript
+    //     var randomNumber = Math.floor(Math.random() * (200 - 20 + 1)) + 20;
+    //     //console.log(i + " || " + randomNumber);
+    //     if (randomNumber % 2 === 0) {
+    //         console.log(randomNumber + " is even.");
+    //     } else {
+    //         console.log(randomNumber + " is odd.");
+    //     }
+    // }
 
 })();
--- js/while.js
+++ js/while.js
@@ -10,24 +10,25 @@
 
         // Generate total # of cones (50 - 100)
     // Generate # of cones sold this round (2 - 6)
+    // Console log cones sold with do while
     // Decide if there are enough cones to sell
     // Subtract sold from total cones
     // Decide if there are still cones available
 
-    // var allCones = Math.floor(Math.random() * 50) + 50;
-    // console.log("You need to sell " + allCones + " cones today.");
-    // while (allCones > 0) {
-    //     var conesThisRound = Math.floor(Math.random() * 5) + 1;
-    //     console.log("\tMay I please have " + conesThisRound + " cones?");
-    //     if (conesThisRound > allCones) {
-    //         console.log("I'm sorry, I can't sell you " + conesThisRound + " cones, I only have " +
-    //         allCones + " cones left.");
-    //         continue;
-    //     }
-    //     allCones = allCones - conesThisRound;
-    //     console.log("You have " + allCones + " cones remaining.");
-    // }
-    // console.log("You've sold all your cones! Time to go home.");
+    var allCones = Math.floor(Math.random() * 50) + 50;
+    console.log("You need to sell " + allCones + " cones today.");
+    do {
+        var conesThisRound = Math.floor(Math.random() * 5) + 1;
+        if (conesThisRound > allCones) {
+            console.log ("Cannot sell you " + conesThisRound + " I only have " +
+            allCones + " cones left.");
+            continue;
+        }
+        console.log(conesThisRound + " cones sold...")
+        allCones = allCones - conesThisRound;
+        //console.log("\tYou still need to sell " + allCones + " more cones.");
+    } while (allCones > 0);
+     console.log("Yay! I sold them all.");
 
 })();
 
