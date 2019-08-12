ce66ceb6333c82d9b8ebc71a9a20f02bf8e1d787
John Cadena <cadena.john.85@gmail.com>
2019-08-12 15:37:57 -0500

    complete for_loops.js

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,26 +1,73 @@
 (function() {
     "use strict";
 
+    //Problem #2
+
     function showMultiplicationTable(someNumber) {
         for (var i = 1; i <=10; i++) {
             console.log(someNumber + " * " + i + " = " + (someNumber * i));
         }
     }
 
+    //Problem #3
+
     //for loop runs ten times
     //each time, a new random # 20 - 200 is generated
     //each time, determine if random is even or odd
 
-    // for (var i = 0; i <10; i++) {
+    // for (var j = 0; j <10; j++) {
     //     //Generating random numbers where minimum is not 1:
     //     // https://stackoverflow.com/questions/4959975/generate-random-number-between-two-numbers-in-javascript
     //     var randomNumber = Math.floor(Math.random() * (200 - 20 + 1)) + 20;
-    //     //console.log(i + " || " + randomNumber);
     //     if (randomNumber % 2 === 0) {
     //         console.log(randomNumber + " is even.");
     //     } else {
     //         console.log(randomNumber + " is odd.");
     //     }
     // }
+
+
+    //Problem #4
+
+    // for (var k = 1; k < 10; k++) {
+    //     console.log(k.toString().repeat(k));
+    // }
+
+
+            // for (var j = 1; j <10; j++) {
+            //     // convert j to a string
+            //     var counterString = j.toString() + "string";
+            //     // create a string variable that strings together chars counter-# of times
+            //     for (var counter = 1; )
+            //     //console.log the string
+            //     console.log(j);
+            //     console.log(counterString);
+            // }
+
+            // for (var j = 1; j <10; j++) {
+            //     console.log(j);
+            //     var jAsString = j.toString();
+            //     var counterString;
+            //     var counter = 0;
+            //     //var k = j;
+            //     while (counter < j) {
+            //         counterString = jAsString + jAsString;
+            //     }
+            //     console.log(counterString);
+            // }
+
+            // function makeCounterString (someNumber) {
+            //     var jAsString = someNumber.toString();
+            //     for (someNumber)
+            //     var counterString =
+            // }
+
+    //Problem #5
+
+    // for (var m = 100; m > 0; m -= 5 ) {
+    //     console.log(m);
+    // }
+
+
 
 })();
--- js/while.js
+++ js/while.js
@@ -15,20 +15,20 @@
     // Subtract sold from total cones
     // Decide if there are still cones available
 
-    var allCones = Math.floor(Math.random() * 50) + 50;
-    console.log("You need to sell " + allCones + " cones today.");
-    do {
-        var conesThisRound = Math.floor(Math.random() * 5) + 1;
-        if (conesThisRound > allCones) {
-            console.log ("Cannot sell you " + conesThisRound + " I only have " +
-            allCones + " cones left.");
-            continue;
-        }
-        console.log(conesThisRound + " cones sold...")
-        allCones = allCones - conesThisRound;
-        //console.log("\tYou still need to sell " + allCones + " more cones.");
-    } while (allCones > 0);
-     console.log("Yay! I sold them all.");
+    // var allCones = Math.floor(Math.random() * 50) + 50;
+    // console.log("You need to sell " + allCones + " cones today.");
+    // do {
+    //     var conesThisRound = Math.floor(Math.random() * 5) + 1;
+    //     if (conesThisRound > allCones) {
+    //         console.log ("Cannot sell you " + conesThisRound + " I only have " +
+    //         allCones + " cones left.");
+    //         continue;
+    //     }
+    //     console.log(conesThisRound + " cones sold...")
+    //     allCones = allCones - conesThisRound;
+    //     //console.log("\tYou still need to sell " + allCones + " more cones.");
+    // } while (allCones > 0);
+    //  console.log("Yay! I sold them all.");
 
 })();
 
