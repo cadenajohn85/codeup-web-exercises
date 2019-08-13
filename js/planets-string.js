f3c38672fb9ea1323b322114caa1262e37268ebf
John Cadena <cadena.john.85@gmail.com>
2019-08-13 14:30:32 -0500

    complete initial draft of planets-string.js and split-join.html

--- js/planets-string.js
+++ js/planets-string.js
@@ -11,7 +11,9 @@
      * console.log planetsArray to check your work
      */
 
-    // console.log(planetsArray);
+    planetsArray = planetsString.split("|");
+
+    console.log(planetsArray);
 
     /**
      * TODO:
@@ -23,5 +25,21 @@
      * list. You will need an opening AND closing <ul> tags around the entire
      * string, and <li> tags around each planet.
      */
+
+    var planetsStringWithBreaks = planetsArray.join("<br>");
+    console.log(planetsStringWithBreaks);
+
+    //Add necessary tags in between list items
+
+    var planetsUnorderedList = planetsArray.join("</li><li>");
+    console.log(planetsUnorderedList);
+
+    //Add necessary tags at beginning of list and 1st list item
+    planetsUnorderedList = "<ul><li>" + planetsUnorderedList;
+    console.log(planetsUnorderedList);
+
+    //Add necessary tags at end of list and list items
+    planetsUnorderedList += "</li></ul>";
+    console.log(planetsUnorderedList);
 
 })();
